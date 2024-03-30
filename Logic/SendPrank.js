b8215655eebb87bd7a7a412846ba5534706e59f0
Matheqs <matheqs@outlook.com>
2024-03-30 17:02:16 -0300

    This is my website

---
+++ Logic/SendPrank.js
@@ -0,0 +1,6 @@
+var enviar = document.getElementById("sendButton"); /* cria uma váriavel que representa o botão de enviar o formulário*/
+enviar.addEventListener("click", function(){ /* causa a função de criar um alerta sempre que a variável for clicada (o botão no caso)*/
+    alert("ENGANEI O BOBO NA CASCA DO OVO! Infelizmente esse botão é de mentirinha e nada foi enviado :( mas nao chore ainda, você pode entrar em contato comigo pelas minhas redes sociais: @Matheqs no instagram ou Matheus Barbosa no LinkedIn, ou envie um email para matheqs@gmail.com (pode ser um pix também, não vou reclamar) esses e demais links para contato você encontra na aba contato do site GIFs Desmotivacinais, projeto encontrado na guia Projetos desse mesmo site! Até breveee <3")
+})
+
+/* a Lógica acima foi pensada para responder ao click do botão enviar do formulário, um alerta abre na página com a mensagem referente*/
